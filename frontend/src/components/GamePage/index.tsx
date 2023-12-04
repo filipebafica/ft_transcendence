b096d71b5a522d83a56d84ebd740d8992a062bff
jdanelon <jdanelon@student.42sp.org.br>
2023-12-03 23:33:59 -0300

    styles to react pong/fixed vertical collisions

--- frontend/src/components/GamePage/index.tsx
+++ frontend/src/components/GamePage/index.tsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react'
-import styles from './style.module.css'
+import React, { useState } from "react";
+import styles from "./style.module.css";
 
-import LocalGame from './LocalGame'
-import RemoteGame from './RemoteGame'
+import LocalGame from "./LocalGame";
+import RemoteGame from "./RemoteGame";
 
 interface GamePageProps {
-  // Define the props for the GamePage component here
+	// Define the props for the GamePage component here
 }
 
 function GamePage(props: GamePageProps) {
-  const [game, setGame] = useState<boolean>(true);
+	const [game, setGame] = useState<boolean>(true);
 
-  const handleSwitchGame = () => {
-    setGame(!game);
-  };
+	const handleSwitchGame = () => {
+		setGame(!game);
+	};
 
-  return (
-    <div>
-      {game ? <LocalGame /> : <RemoteGame/>}
-      <button onClick={handleSwitchGame}>Switch Game</button>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			{game ? <RemoteGame /> : <LocalGame />}
+			<button onClick={handleSwitchGame} className={styles.button}>
+				Switch Game
+			</button>
+		</div>
+	);
 }
 
 export default GamePage;
