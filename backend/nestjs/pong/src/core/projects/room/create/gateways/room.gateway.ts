e70fe128d963ca32390a45460f7667d6f3bafa31
Diego Cisneros <diego@MacBook-Pro.local>
2023-12-14 23:55:47 -0500

    update

--- backend/nestjs/pong/src/core/projects/room/create/gateways/room.gateway.ts
+++
@@ -1,7 +0,0 @@
-export default interface CreateGateway {
-    create(
-        userId: number,
-        roomName: string,
-        type: string
-    ): void;
-}
--- backend/nestjs/pong/src/core/projects/room/dependencies/gateways/get.rooms.gateway.ts
+++
@@ -1,7 +0,0 @@
-import RoomDTO from "../dtos/room.dto";
-
-export default interface GetRoomsGateway {
-    get(): RoomDTO[];
-
-    getByUserId(userId: number): RoomDTO[];
-}
