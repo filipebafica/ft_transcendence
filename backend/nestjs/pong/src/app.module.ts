1518b6ac50a00dee97f63e08efa6324777afefe5
Filipe Báfica <filipe.bafica@arquivei.com.br>
2023-12-16 14:42:24 -0300

    StatusGateway

--- backend/nestjs/pong/src/app.module.ts
+++ backend/nestjs/pong/src/app.module.ts
@@ -5,9 +5,16 @@ import { ChatModule } from './app/projects/chat/chat.module';
 import { GameModule } from './app/projects/game/game.module';
 import { RoomModule } from './app/projects/room/room.module';
 import { FriendModule } from './app/projects/friend/friend.module';
+import { StatusModule } from './app/projects/status/status.module';
 
 @Module({
-  imports: [ChatModule, RoomModule, FriendModule, GameModule],
+  imports: [
+    ChatModule,
+    RoomModule,
+    FriendModule,
+    StatusModule,
+    GameModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
