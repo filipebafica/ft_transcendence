5112a46e57e21bcc6e66ddc00d0994c5676e893f
Diego Cisneros <diego@MacBook-Pro.local>
2023-12-16 21:56:12 -0500

    Update

--- frontend/src/socket/index.tsx
+++ frontend/src/socket/index.tsx
@@ -10,3 +10,5 @@ export const chatSocket = io(URL!, { path: '/websocket/chat' })
 export const roomSocket = io(URL!, { path: '/websocket/room' })
 
 export const gameSocket = io(URL!, { path: '/websocket/game' })
+
+export const friendsStatusSocket = io(URL!, { path: '/websocket/friend' })
