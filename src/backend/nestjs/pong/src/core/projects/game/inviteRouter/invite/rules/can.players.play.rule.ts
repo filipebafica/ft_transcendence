336b6b0162ef44e6071208ccdcad63d96c9168ea
victor-ln <vlima-nu@student.42sp.org.br>
2023-12-26 18:32:53 -0300

    Docker everything

---
+++ src/backend/nestjs/pong/src/core/projects/game/inviteRouter/invite/rules/can.players.play.rule.ts
@@ -0,0 +1,36 @@
+import { GameHistoryRepository } from "../../../shared/interfaces/game.history.repository";
+import { InvitationRepository } from "../../../shared/interfaces/invitation.repository";
+import { QueueInterface } from "../../../shared/interfaces/queue.interface";
+import { Request } from "../dtos/request.dto";
+
+export class CanPlayersPlayRule {
+	constructor(
+		private gameHistory: GameHistoryRepository,
+		private WaitingQueue: QueueInterface,
+		private invitationRepository: InvitationRepository,
+	) {}
+
+	public async apply(request: Request): Promise<boolean> {
+		if (await this.gameHistory.getRunningGameByPlayerId(request.message.data.to) != null) {
+			return false;
+		}
+
+		if (await this.gameHistory.getRunningGameByPlayerId(request.message.data.from) != null) {
+			return false;
+		}
+
+		if (await this.WaitingQueue.isOnQueue(request.message.data.to) != false) {
+			return false;
+		}
+
+		if (await this.WaitingQueue.isOnQueue(request.message.data.from) != false) {
+			return false;
+		}
+
+		if (await this.invitationRepository.hasOpenInvite(request.message.data.to, request.message.data.from) != false) {
+			return false;
+		}
+
+		return true;
+	}
+}
